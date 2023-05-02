944c3540e636b0fefdf2cab3d311e7ec987223f7
Евеліна Федоренко <ipz192_feo@student.ztu.edu.ua>
2023-05-02 18:00:53 +0300

    adding home screen

---
+++ screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import { useState } from 'react'
+
+export default function HomeScreen({navigation}) {
+    return (
+        <View>
+            <Text> Home Screen </Text>
+
+        </View>
+    );
+}
